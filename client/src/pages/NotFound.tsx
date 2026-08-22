fd3867222d1ce2e70a07b6be901eed8249af7de5
Zobeir-Rigi <rigi.zobir@yahoo.com>
2026-08-22 17:34:14 +0100

    404 message fixed

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -14,7 +14,7 @@ export function NotFound() {
         <div className="max-w-lg text-center">
           <p className="font-sans text-sm font-bold text-accent">404 Error </p>
           <p className="mt-5 font-sans text-base leading-7 text-muted">
-            This page is currently under development and will be available soon.
+            The page you're looking for doesn't exist or may have been moved.
           </p>
 
           <Button className="mt-8" onClick={() => navigate("/")}>
